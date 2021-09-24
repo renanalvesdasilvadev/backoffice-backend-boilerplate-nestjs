776a161bd97db59be4d573bbd7045b82e32232c0
Renan Silva <renan.silva@kabum.com.br>
2021-09-23 23:21:47 -0300

    refactor

--- src/core/repositories/v1/user.repository.interface.ts
+++ src/core/repositories/v1/user.repository.interface.ts
@@ -1,4 +1,4 @@
-import { User } from "../../domains/v1/user";
+import { User } from "src/core/domains/v1/user/user";
 import { IRepository } from "./repository.interface";
 
 export interface IUserRepository extends IRepository<User> {
