9f6edbbe933dbdbb1dc3ee9b5d52c3f7b749853f
Levi Costa <elnata.costa@kabum.com.br>
2021-10-27 17:14:48 -0300

    commit data

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const S3Service = require( './src/service/s3Service')
+const ZipService = require('./src/service/zipService')
+const FileSystemService = require('./src/service/fileSystemService')
+
 module.exports.handler = async (event) => {
   console.log(event);
   const response = {
@@ -8,4 +12,24 @@ module.exports.handler = async (event) => {
     };
   console.log({response: response});
   return response;
+
+  let file;
+
+  try {
+
+    const zipService = new ZipService();
+    const fileSystemService = new FileSystemService();
+    const s3Service = new S3Service({
+      accessKeyId: this.process.S3_ACCESS_KEY,
+      secretAccessKey: this.process.S3_ACCESS_SECRET,
+    });
+
+    zipService.setInputFilePath(file);
+    zipService.setOutPutFilePath('tabela_contigencia.csv');
+    await zipService.unzip();
+    s3Service.initS3();
+
+  } catch (error) {
+    console.log(error);
+  }
 };
